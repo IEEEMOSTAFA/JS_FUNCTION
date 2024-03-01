5ce6ca33b8b0f8b0957ffad687a91c9bc16303c6
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-01 20:27:30 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,20 @@
+const price = 45;
+
+// function declare
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk tower the switch');
+    console.log('Click the switch of the Fan');
+}
+// call the function
+fanOffKor();
+
+
+// function name() {}
+function brushTeeth() {
+    console.log('Pick up the brush');
+    console.log('add paste');
+    console.log('2 min goco and gosco and fena felo');
+
+}
+brushTeeth();
---
+++ parameter.js
@@ -0,0 +1,37 @@
+function vaatkhao(){
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate e khabar dhabar nao');
+    console.log('moja kore khao');
+}
+
+vaatkhao();
+
+// 3-------> 3*3 = 9
+// 11-------> 11*11 = 121
+
+function square (number) {
+    console.log('value of the parameter',number);
+    const borgo = number * number;
+    console.log("Square of the number parameter",borgo)
+
+}
+square(4);
+console.log('------------------')
+square(12);
+square(13);
+
+console.log('------------------')
+function add(num1,num2){
+    const sum = num1 + num2;
+    console.log("Sum of the Two Number ",sum);
+}
+add(1,99);
+
+console.log('------- all number -----------')
+function addAll(a,b,c,d,e){
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+
+addAll(2,2,2,2,2);
